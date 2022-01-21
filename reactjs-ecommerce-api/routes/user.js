4474f86c8bded8e58887d129abb20bcad42f9d7d
Hao <54834780+HaoYeChen@users.noreply.github.com>
2022-01-21 14:10:58 +0100

    update

--- reactjs-ecommerce-api/routes/user.js
+++ reactjs-ecommerce-api/routes/user.js
@@ -1,5 +1,19 @@
+const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+
 const router = require("express").Router();
 
+//UPDATE
+router.put("/:id", verifyTokenAndAuthorization, (req, res)=> {
+    if(req.body.password){
+        password: CryptoJS.AES.encrypt(
+            req.body.password,
+            process.env.PASS_SEC
+          ).toString(),
+    }
+}
 
 
+
+
+)
 module.exports = router;
